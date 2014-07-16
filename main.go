0a21338d4fdf5942f41b2f852ee27e5da420d3a6
赵卫国 <programfan.info@gmail.com>
2014-07-16 16:28:11 +0800

    update

--- main.go
+++ main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+//	"github.com/samuel/go-zookeeper/zk"
 )
 
+func get(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+}
+
 func main() {
-	fmt.Println("hello")
+
+	http.HandleFunc("/get", get)
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+
 }
 
 
